4ed0e9dd590b9712442626de3cb1c0aea34b8154
jinfanghuang <2283785225@qq.com>
2024-03-14 17:34:22 +0800

    feat: Add File Example

--- doc/file.js
+++ doc/file.js
@@ -1,18 +1,30 @@
-const {Global, File} = miniCore;
+const { Global, File } = miniCore;
+const { Space, Icon } = antd;
+const { View } = tarojsComponents;
+
 const BaseExample = () => {
-    return <Global preset={{
-        apis: {
-            file: {
-                getFileUrl: {
-                    loader: ({params}) => {
-                        return "https://attachment.test.fatalent.cn/attachment/Q0ol94kBBZgnCXyZKG1Y.jpg?Expires=1700814537&OSSAccessKeyId=LTAI5tAfbu2aBppB3jMj1kMt&Signature=KcJHlI8FDYMolFQFxacGzhoaA1A%3D";
-                    }
-                }
-            }
+  return <Global preset={{
+    apis: {
+      file: {
+        getFileUrl: {
+          loader: ({ params }) => {
+            return "https://attachment.test.fatalent.cn/attachment/Q0ol94kBBZgnCXyZKG1Y.jpg?Expires=1700814537&OSSAccessKeyId=LTAI5tAfbu2aBppB3jMj1kMt&Signature=KcJHlI8FDYMolFQFxacGzhoaA1A%3D";
+          }
         }
-    }}>
-        <File value="xxxxx" originalName="预览文件.jpg"/>
-    </Global>;
+      }
+    }
+  }}>
+    <Space direction={"vertical"} size={30}>
+      <Space direction={"vertical"}>
+        <View>基础用法</View>
+        <File value="xxxxx" originalName="预览文件.jpg" />
+      </Space>
+      <Space direction={"vertical"}>
+        <View>修改文件图标</View>
+        <File value="xxxxx" originalName="预览文件.jpg" icon={<Icon className="iconfont" type="tianjia" />} />
+      </Space>
+    </Space>
+  </Global>;
 };
 
-render(<BaseExample/>);
+render(<BaseExample />);
