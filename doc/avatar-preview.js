dbf19ae4dc613be704348f55881a45dc72b4949d
jinfanghuang <2283785225@qq.com>
2024-03-14 14:19:21 +0800

    feat: Add AvatarPreview Example

---
+++ doc/avatar-preview.js
@@ -0,0 +1,27 @@
+const { AvatarPreview } = miniCore;
+const { Space, Button } = antd;
+const { View, showToast } = tarojsComponents;
+
+const demoAvatarImages = "https://avatars.githubusercontent.com/u/37367461?v=4";
+
+const BaseExample = () => {
+  return (
+    <Space direction="vertical" size={30}>
+      <Space direction={"vertical"}>
+        <View>基础用法</View>
+        <AvatarPreview value={demoAvatarImages} />
+      </Space>
+      <Space direction={"vertical"}>
+        <View>点击事件</View>
+        <AvatarPreview
+          value={demoAvatarImages}
+          onClick={() => {
+            showToast({icon: 'none', title: '点击事件。。。'})
+          }}
+        />
+      </Space>
+    </Space>
+  );
+};
+
+render(<BaseExample />);
--- src/AvatarPreview/index.js
+++ src/AvatarPreview/index.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import {withOssFile} from '../OssFile';
-import {Avatar} from "@kne/antd-taro";
+import React from "react";
+import { withOssFile } from "../OssFile";
+import { Avatar } from "@kne/antd-taro";
 
-
-const AvatarPreview = withOssFile(({className, mode = "aspectFill", size, url, ...props}) => {
-    return <Avatar {...props} src={url}/>;
+const AvatarPreview = withOssFile(({ className, mode = "aspectFill", size, url, ...props }) => {
+  return <Avatar {...props} src={url} />;
 });
 
-
-export default AvatarPreview
+export default AvatarPreview;
