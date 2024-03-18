93e23379ccb60be567c3906975ef05372f892950
jinfanghuang <2283785225@qq.com>
2024-03-18 15:35:08 +0800

    feat: Add Warning Example

--- doc/warning.js
+++ doc/warning.js
@@ -1,6 +1,36 @@
-const {Warning} = miniCore;
+const { Warning } = miniCore;
+const { Space } = antd;
+const { View } = tarojsComponents;
+
 const BaseExample = () => {
-    return <Warning>哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</Warning>;
+  return (
+    <Space direction={'vertical'} size={30}>
+      <Space direction={'vertical'}>
+        <View>基础用法</View>
+        <Warning>哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</Warning>
+      </Space>
+      <Space direction={'vertical'}>
+        <View>类型</View>
+        <Space direction={'vertical'}>
+          {
+            ['success', 'info', 'error', 'warning', ].map(type => (
+              <Warning key={type} type={type}>哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</Warning>
+            ))
+          }
+        </Space>
+      </Space>
+      <Space direction={'vertical'}>
+        <View>文字颜色根据类型改变</View>
+        <Space direction={'vertical'}>
+          {
+            ['success', 'info', 'error', 'warning', ].map(type => (
+              <Warning key={type} type={type} fontColorful>哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</Warning>
+            ))
+          }
+        </Space>
+      </Space>
+    </Space>
+  );
 };
 
-render(<BaseExample/>);
+render(<BaseExample />);
